8f3e34cc743ed769c6a417d9c910c4656c7289df
lukacrnogorac <lcrnog00@fesb.hr>
2019-03-22 13:02:36 +0100

    Second commit

--- Firstproject/Program.cs
+++ Firstproject/Program.cs
@@ -6,7 +6,7 @@ namespace Firstproject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pozdrav svima !");
         }
     }
 }
